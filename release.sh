085c785a779a9cf9080866f14faf6508a98c1793
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2016-05-10 21:24:04 -0400

    update release script.

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
 # Runs before git flow release finish
+to_branch = "without-cython"
 
-git branch -D without-cython
-git push origin --delete without-cython
-git checkout -b without-cython
+git branch -D $to_branch
+git push origin --delete $to_branch
+git checkout -b $to_branch
 
 git rm --cached \*.cpy
 git rm --cached \*.cpx
@@ -26,13 +27,12 @@ git rm --cached -r config
 
 cp config/site.template.cfg .
 cp config/.gitignore .
+cp config/.travis.yml .
 
-git add qr_mumps/solver.py
 git add tests/\*.py
 git add setup.py
 git add --all
 
 git commit -m "c files from last commit in develop"
-git push --set-upstream origin without-cython
+git push --set-upstream origin $to_branch
 git checkout develop
-
